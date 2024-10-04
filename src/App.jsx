42bcafdbd1e372f369558233a4a10e8b1694b492
gab-braga <gabrielbraga0712@gmail.com>
2024-10-04 10:06:09 -0300

    fix: add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,14 @@
 import './App.css';
-import Header from './components/header';
-import Modal from './components/modal';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
-import { useEffect, useState } from 'react';
-import { findAll } from './firebase/firestore';
 
 function App() {
-  const [modal, setModal] = useState(false);
-  const [tasks, setTasks] = useState([]);
-
-  async function loadTasks() {
-    const data = await findAll('tasks', 'date');
-    setTasks(data);
-  }
-
-  function openModal() {
-    setModal(true);
-  }
-
-  function closeModal() {
-    setModal(false);
-  }
-
-  function updateData() {
-    loadTasks();
-  }
-
-  useEffect(() => {
-    loadTasks();
-  }, []);
-
   return (
-    <div className="root">
-      {modal && <Modal close={closeModal} update={updateData} />}
-      <Header action={openModal} />
-      <Home tasks={tasks} update={updateData} />
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,44 @@
 import './style.css';
 import Card from '../../components/card';
+import { useEffect, useState } from 'react';
+import { findAll } from '../../firebase/firestore';
+import Modal from '../../components/modal';
+import Header from '../../components/header';
 
-export default ({ tasks, update }) => {
+export default () => {
+  const [modal, setModal] = useState(false);
+  const [tasks, setTasks] = useState([]);
+
+  async function loadTasks() {
+    const data = await findAll('tasks', 'date');
+    setTasks(data);
+  }
+
+  function openModal() {
+    setModal(true);
+  }
+
+  function closeModal() {
+    setModal(false);
+  }
+
+  function updateData() {
+    loadTasks();
+  }
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
   return (
     <div className="home">
-      {tasks.map((task, idx) => (
-        <Card {...task} key={idx} update={update} />
-      ))}
+      {modal && <Modal close={closeModal} update={updateData} />}
+      <Header action={openModal} />
+      <div className="tasks">
+        {tasks.map((task, idx) => (
+          <Card {...task} key={idx} update={updateData} />
+        ))}
+      </div>
     </div>
   );
 };
