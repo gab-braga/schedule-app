3dc5fdd89dcabb18491ae775ae5d6121d2c7b855
gab-braga <gabrielbraga0712@gmail.com>
2024-10-08 19:28:33 -0300

    style: update style

--- src/components/day/index.jsx
+++ src/components/day/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./style.css";
 
 function formatDayOfWeek(date) {
@@ -9,8 +10,17 @@ function formatDayOfWeek(date) {
 }
 
 export default ({ day }) => {
+    const [isToday, setIsToday] = useState(false);
+
+    useEffect(() => {
+        const today = new Date();
+        const formattedToday = today.toISOString().split('T')[0];
+        const formattedDate = day.toISOString().split('T')[0];
+        setIsToday(formattedToday === formattedDate);
+    }, [day]);
+
     return (
-        <div className="day">
+        <div className={isToday ? "day today" : "day"}>
             <span className="title">
                 {formatDayOfWeek(day)}
             </span>
@@ -46,9 +56,6 @@ export default ({ day }) => {
                     <span className="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</span>
                 </div>
             </div>
-            <button className="btn-new">
-                Novo &#43;
-            </button>
         </div>
     );
 }
